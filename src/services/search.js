49f1be3f6e3fc3950f607bca0bb1304d1af35265
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2022-02-02 12:15:01 +0100

    try to fix services

--- src/services/search.js
+++ src/services/search.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const authService = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}`,
-});
+const API_URL = process.env.REACT_APP_API_URL;
 
 function internalServerError(err) {
   if (err.response && err.response.data && err.response.data.errorMessage) {
@@ -25,9 +23,9 @@ function successStatus(res) {
 }
 
 export function newSearch(credentials) {
-  return authService.post("/album", credentials).then(successStatus).catch(internalServerError);
+  return axios.post(`${API_URL}/album`, credentials).then(successStatus).catch(internalServerError);
 }
 
 export function searchNotFound(credentials) {
-  return authService.post("/song-not-find", credentials).then(successStatus).catch(internalServerError);
+  return axios.post(`${API_URL}/song-not-find`, credentials).then(successStatus).catch(internalServerError);
 }
